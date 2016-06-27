27452021cfbc88d869ce91e552e50ab2fc1fff17
Cory Franks and Tommy Jones <cory+tommy@>
2016-06-27 16:04:52 -0700

    Initial commit

---
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -0,0 +1,33 @@
+package com.epicodus.myrestaurants;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
+    }
+}
